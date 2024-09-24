4ff8c77e794f5920e2e127a83a9b0bb5c31a7344
imhere4thehorse <kaliechang10847@gmail.com>
2024-09-24 13:22:32 -0600

    INCOMPLETE: changed led.c so multiple lights turn on instead of just one port's lights

--- led.c
+++ led.c
@@ -2,10 +2,21 @@
 #include <pigpiod_if2.h>
 // gcc led.c -lpigpiod_if2
 // ./a.out
-int main(){
-	int PI = pigpio_start(NULL,NULL);
-	set_mode(PI, 27, PI_OUTPUT);
 
+int main(){
+	int GPIO
+	int port1 = 27;
+	int port2 = 25;
+	int port3 = 23;
+	int port4 = 21;
+
+	int PI = pigpio_start(NULL,NULL);
+	
+	//iterates through all GPIO to see which ports are connected to which GPIO numbers
+	
+	//for(GPIO = 1; GPIO<=39; GPIO++){
+	set_mode(PI, 27, PI_OUTPUT);
+	
 	for(int i = 0; i <= 10; i++){
 		gpio_write(PI,27,1);
 		time_sleep(1);
